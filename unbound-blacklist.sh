84d815bc882137ad0c6e79f037bd74a7f2326702
k0nsl <i.am@k0nsl.org>
2016-08-02 22:55:14 +0200

    Create unbound-blacklist.sh
    I found this script on `calomel.org` sometime ago and it fit my purpose at the time, and it still does. It will fetch garbage sites from four sources and compile them into a file called `automated-blocklist.conf` which is included by my custom compiled unbound instance.
    
    I have only done minor changes to it. You would need to adjust the paths, etc,. Set a crontab entry, etc,. After that, you're done!
    
    It may be a bit rough, but it works fine ;)

---
+++ unbound-blacklist.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+## Clean up any stale tempfile
+[[ -f /tmp/hosts.working ]] && rm -f /tmp/hosts.working
+
+## Awk regex to be inverse-matched as whitelist
+whitelist='/(k0nsl.org|goy.su)/'
+
+## URLs of Ad Blacklists to Use
+blacklist=('http://winhelp2002.mvps.org/hosts.txt' \
+           'http://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext' \
+           'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts' \
+           'https://adaway.org/hosts.txt')
+
+## Fetch all Blacklist Files
+for url in $blacklist; do
+    curl --silent -A "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36" $url >> "/tmp/hosts.working"
+done
+
+## Process Blacklist, Eliminiating Duplicates, Integrating Whitelist, and Converting to unbound format
+awk -v whitelist="$whitelist" '$1 ~ /^127\.|^0\./ && $2 !~ whitelist {gsub("\r",""); print tolower($2)}' /tmp/hosts.working | sort | uniq | \
+awk '{printf "local-zone: \"%s\" redirect\n", $1; printf "local-data: \"%s. A 0.0.0.0\"\n", $1}' > /home/unbound/unbound-compiled/etc/unbound/conf/automated-blocklist.conf
+
+# "Restart" unbound ;-)
+/usr/bin/killall unbound
+/home/unbound/unbound-compiled/sbin/unbound -c /home/unbound/unbound-compiled/etc/unbound/unbound.conf
+
+# Clean up tempfile
+rm -f '/tmp/hosts.working'
