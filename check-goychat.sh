5d0bcc14b40018b8b4e393a842575044ccb92048
k0nsl <i.am@k0nsl.org>
2017-07-12 10:55:12 +0200

    Fix swaks syntax

--- check-goychat.sh
+++ check-goychat.sh
@@ -2,9 +2,10 @@
 if /usr/bin/torify /usr/bin/curl -s --head http://vid2x7jyypqblcc4.onion/ | /bin/grep "200 OK" >/dev/null
   then
     /bin/echo "The HTTPd returned a 200 OK." >/dev/null
+    /usr/bin/swaks -t to@domain.tld -f "from@domain.tld" --header "Subject: Goy.Chat [up]" --body "The tor-wrapper for Goy.Chat is available and healthy." --server mail.domain.tld --auth LOGIN --auth-user "user@domain.tld" --auth-password "redacted" -tls -p 587
     exit 0
   else
     /bin/echo "The HTTPd isn't returning a healthy reply." >/dev/null
-    echo "The tor-wrapper for Goy.Chat is currently unavailable." | swaks --to postmaster@k0nsl.org --from "alerts@k0nsl.org" --server mail.domain.tld --auth LOGIN --auth-user "redacted@domain.tld" --auth-password "redacted" -tls
+    /usr/bin/swaks -t to@domain.tld -f "from@domain.tld" --header "Subject: Goy.Chat [down]" --body "The tor-wrapper for Goy.Chat is unavailable — investigate immediately." --server mail.domain.tld --auth LOGIN --auth-user "user@domain.tld" --auth-password "redacted" -tls -p 587
     exit 1
 fi
