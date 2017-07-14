f096abc86a1255f34d9c43709b91f8f7f8107e19
k0nsl <i.am@k0nsl.org>
2017-07-14 02:14:22 +0200

    Cause swaks to be silent (no output, ever)

--- check-goychat.sh
+++ check-goychat.sh
@@ -2,11 +2,11 @@
 if /usr/bin/torify /usr/bin/curl -s --head http://vid2x7jyypqblcc4.onion/ | /bin/grep "200 OK" >/dev/null
   then
     /bin/echo "The HTTPd returned a 200 OK." >/dev/null
-    /usr/bin/swaks -t to@domain.tld -f "from@domain.tld" --header "Subject: Goy.Chat [up]" --body "The tor-wrapper for Goy.Chat is available and healthy." --server mail.domain.tld --auth LOGIN --auth-user "user@domain.tld" --auth-password "redacted" -tls -p 587
+    /usr/bin/swaks --silent 3 -t to@domain.tld -f "from@domain.tld" --header "Subject: Goy.Chat [up]" --body "The tor-wrapper for Goy.Chat is available and healthy." --server mail.domain.tld --auth LOGIN --auth-user "user@domain.tld" --auth-password "redacted" -tls -p 587
     exit 0
   else
     /bin/echo "The HTTPd isn't returning a healthy reply." >/dev/null
-    /usr/bin/swaks -t to@domain.tld -f "from@domain.tld" --header "Subject: Goy.Chat [down]" --body "The tor-wrapper for Goy.Chat is unavailable — investigate immediately." --server mail.domain.tld --auth LOGIN --auth-user "user@domain.tld" --auth-password "redacted" -tls -p 587
+    /usr/bin/swaks --silent 3 -t to@domain.tld -f "from@domain.tld" --header "Subject: Goy.Chat [down]" --body "The tor-wrapper for Goy.Chat is unavailable — investigate immediately." --server mail.domain.tld --auth LOGIN --auth-user "user@domain.tld" --auth-password "redacted" -tls -p 587
     /usr/bin/curl -s https://domain.tld/goychat-webhook.php
     exit 1
 fi
